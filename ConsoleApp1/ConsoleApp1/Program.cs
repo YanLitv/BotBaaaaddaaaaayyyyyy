5b656c5de54aa04cb4d1d475780b721375b8eaa1
Влад <43582635+object10110@users.noreply.github.com>
2019-03-06 19:07:40 +0200

    Added listening

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static TelegramBotClient botClient;
+        static void Main(string[] args)
+        {
+            botClient = new TelegramBotClient("745606871:AAGqN588lpsuNDJlzeSx3FFZz1RHgS9VCtU");
+            botClient.OnMessage += BotClient_OnMessage;
+            botClient.StartReceiving();
+            Console.Read();
+        }
+
+        private static void BotClient_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            Console.WriteLine("Badaj!");
+        }
+    }
+}
